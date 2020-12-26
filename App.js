8a02e07e15bb516471761ce7b1e2f865362f2ac4
OctoM <orders@octom.ru>
2020-12-27 06:30:32 +0900

    React init

---
+++ App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button 
+        title="1223"
+        onPress={() => {}}  
+      />
+      <StatusBar style="auto" />
+    </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
